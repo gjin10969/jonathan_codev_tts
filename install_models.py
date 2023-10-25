548536f603be2a49cbdbe9bc9d1a3513649760a1
gjin10969 <113190973+gjin10969@users.noreply.github.com>
2023-10-25 19:06:40 +0800

    Update install_models.py

--- install_models.py
+++ install_models.py
@@ -16,9 +16,9 @@
 #fine
 !gdown --folder https://drive.google.com/drive/folders/1tLwY4wTgASpA6CVxGKduZ8Qxso0Rwtnb?usp=sharing...somefileid.. -O /root/jonathan_codev_tts/bark-model
 
-!aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/gjin10969/rvc-model-sample/resolve/main/Aoyagisan/AoyagiVoice2YS.Speech.pth -d /root/jonathan_codev_tts/weights/eli -o eli_sterio.pth
+!aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/gjin10969/rvc-model-sample/resolve/main/eli/eli_sterio.pth -d /root/jonathan_codev_tts/weights/eli -o eli_sterio.pth
 !aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/gjin10969/rvc-model-sample/resolve/main/Taka/Taka_last.pth -d /root/jonathan_codev_tts/weights/Taka -o Taka_last.pth
 !aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/gjin10969/rvc-model-sample/resolve/main/hanayo/hanayo.pth -d /root/jonathan_codev_tts/weights/hanayo -o hanayo.pth
-
+!aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/gjin10969/rvc-model-sample/resolve/main/Aoyagisan/AoyagiVoice2YS.Speech.pth -d /root/jonathan_codev_tts/weights/Aoyagisan -o Aoyagisan.pth
 !aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/gjin10969/rvc-model-sample/resolve/main/suguirasan/suguirasan_voice.pth -d /root/jonathan_codev_tts/weights/suguirasan -o suguirasan_voice.pth
 
